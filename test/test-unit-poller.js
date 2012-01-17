48a8800f69114d13e6b92cd85d6c6a99c5ffb7ff
Jonas Dohse <jonas@dohse.ch>
2012-01-17 18:31:50 +0100

    Add test for Poller

---
+++ test/test-unit-poller.js
@@ -0,0 +1,161 @@
+var net = require('net');
+
+var Poller = require('../lib/poller');
+
+var sinon = require('sinon');
+
+
+exports.setUp = function(cb) {
+  this.net_Socket = net.Socket;
+
+
+  this.socket = {
+    on: sinon.stub(),
+    connect: sinon.stub(),
+    destroy: sinon.stub()
+  }
+  net.Socket = sinon.stub();
+  net.Socket.returns(this.socket);
+
+  this.poller = new Poller([ 1337 ]);
+
+  cb(null);
+};
+
+exports.tearDown = function(cb) {
+  net.Socket = this.net_Socket;
+  cb(null);
+};
+
+var filterOn = function(event) {
+  var arr = [].slice.call(arguments, 1);
+  return function(curEvent, handler) {
+    if (curEvent === event) {
+      process.nextTick(function() {
+        handler.apply(null, arr);
+      });
+    }
+  };
+};
+
+var U;
+
+exports['Poller.poll()'] = U = {};
+
+U['with successfull connect'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  socket.on = sinon.spy(filterOn('connect'));
+
+  self.poller.poll(function(err) {
+    test.ifError(err);
+    test.ok(socket.destroy.calledOnce);
+
+    test.equals(self.poller.socket, null);
+
+    test.done();
+  });
+
+  test.ok(socket.on.calledWith('connect'));
+  test.ok(socket.on.calledWith('error'));
+
+  test.ok(socket.connect.calledOnce);
+  test.ok(socket.connect.calledWith(1337));
+};
+
+U['with failed connect'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  var error = new Error('Dummy error');
+  socket.on = sinon.spy(filterOn('error', error));
+
+  self.poller.poll(function(err) {
+    test.equal(err, error);
+
+    test.done();
+  });
+};
+
+exports['Poller.wait()'] = U = {};
+
+U['returns'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  socket.on = sinon.spy(filterOn('connect'));
+
+  self.poller.wait(function(err) {
+    test.ifError(err);
+
+    test.done();
+  });
+};
+
+U['is cancelable'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  var error = new Error('Dummy error');
+  socket.on = sinon.spy(filterOn('error'));
+
+  self.poller.wait(function(err) {
+    test.ok(err instanceof Error);
+
+    test.done();
+  });
+
+  self.poller.cancel();
+};
+
+U['works with multiple callbacks'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  socket.on = sinon.spy(filterOn('connect'));
+
+  var firstCall = sinon.stub();
+  self.poller.wait(firstCall);
+
+  self.poller.wait(function(err) {
+    test.ifError(err);
+
+    test.ok(firstCall.calledOnce);
+    test.ok(firstCall.calledWith());
+
+    test.done();
+  });
+};
+
+var onEmitErrorEmitConnect = function() {
+  var connectSeen = 0;
+  return function(event, handler) {
+    if (event === 'connect') {
+      connectSeen++;
+    }
+    if (connectSeen < 2 && event === 'error') {
+      process.nextTick(function() {
+        handler(new Error('Dummy error'));
+      });
+    } else if (connectSeen >= 2 && event === 'connect') {
+      process.nextTick(handler);
+    }
+  };
+};
+
+U['works with delay'] = function(test) {
+  var self = this;
+
+  var socket = self.socket;
+  socket.on = sinon.spy(onEmitErrorEmitConnect());
+
+  self.poller.wait(function(err) {
+    test.ifError(err);
+
+    test.equal(socket.on.callCount, 4);
+
+    test.done();
+  });
+};
+
