4ed8310174d3d8a11d544209720836e9fa115eee
Jonas Dohse <jonas@dohse.ch>
2012-01-23 12:07:06 +0100

    Export Poller

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Executable = require('./lib/executable');
+exports.Poller = require('./lib/poller');
 exports.Service = require('./lib/service');
 exports.ServiceFactory = require('./lib/service_factory');
